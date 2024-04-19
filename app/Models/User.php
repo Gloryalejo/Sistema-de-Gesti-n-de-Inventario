aa147452f85c0bcd8a61205222316e40b1b01f1d
Omar Sánchez <omaredu.02@gmail.com>
2024-04-19 23:18:25 +0000

    passport installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,16 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
 
 class User extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
-        'name', 
-        'last_name', 
-        'role_id', 
-        'email', 
+        'name',
+        'last_name',
+        'role_id',
+        'email',
         'password',
     ];
 }
