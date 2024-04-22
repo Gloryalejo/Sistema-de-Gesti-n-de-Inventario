acc2efac5f3a343e4e2ffd09fe1700b66b49356a
Omar Sánchez <omaredu.02@gmail.com>
2024-04-22 23:26:45 +0000

    Authentification

---
+++ app/Http/Middleware/ValidateToken.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
+class ValidateToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->bearerToken();
+
+        if (empty($token)) {
+            return response()->json([
+                'message' => 'Missing token',
+                'data' => json_decode('{}'),
+                'status' => 401
+            ], 401);
+        }
+
+        $user = User::fromToken($token);
+
+        if (empty($user)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => json_decode('{}'),
+                'status' => 401
+            ], 401);
+        }
+
+        $request->attributes->set('user', $user);
+        return $next($request);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 
 class User extends Model
@@ -20,4 +22,40 @@ class User extends Model
         'email',
         'password',
     ];
+
+    public static function jwtSecret(): string
+    {
+        return env('JWT_SECRET') ?? "secret";
+    }
+
+    public function encodeToken(): string
+    {
+        $payload = ["user_id" => $this->id];
+        return JWT::encode($payload, $this->jwtSecret(), "HS256");
+    }
+
+    public static function decodeToken(string $token): object
+    {
+        return JWT::decode($token, new Key(User::jwtSecret(), 'HS256'));
+    }
+
+    public static function fromToken(string $token): User|null
+    {
+        if (User::validateToken($token) === false) {
+            return null;
+        }
+
+        $decoded = User::decodeToken($token);
+        return User::find($decoded->user_id);
+    }
+
+    public static function validateToken(string $token): bool
+    {
+        try {
+            User::decodeToken($token);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 }
